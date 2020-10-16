1d784afc59b00d0e99e07d4436faee678ba711d0
JoshBayne <josh.bayne222@gmail.com>
2020-10-16 14:05:08 -0500

    Add list to site

---
+++ scripts/listLinks.js
@@ -0,0 +1,32 @@
+// These are the links for the Server options
+$('a.servers').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=poweredge+r520&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=poweredge+520');
+    window.open('https://www.ebay.com/sch/i.html?_nkw=poweredge+r720&_sop=12');
+    window.open('https://www.racks4server.com/Storage-System/3U-Rackmount-Case-RPC-3116-3U-Server-Case-w-16-Hot-Swappable-SATASAS-Drive-Bay.html');
+});
+
+// These are the links for the NAS / Data Storage options
+$('a.nas').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.ebay.com/sch/i.html?_nkw=powervault+md1000&_sop=12');
+    window.open('https://www.amazon.com/Synology-bay-DiskStation-DS418-Diskless/dp/B075N17DM6');
+    window.open('https://www.amazon.com/Synology-Bay-DiskStation-DS420-Diskless/dp/B087Z3N428');
+});
+
+// These are the links for the Cameras
+$('a.cameras').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=sony+a6100&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=sony+a6000');
+    window.open('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1311&_nkw=fujifilm+x-t30&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=sony+a6100');
+});
+
+// These are the links for the hats
+$('a.hats').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.ebbets.com/collections/ballcaps/products/tokyo-kyojin-giants-1940-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/hiroshima-carp-1958-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/bismarck-churchills-1935-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/brooklyn-bushwicks-1949-ballcap');
+    window.open('https://www.ebbets.com/collections/ballcaps/products/salt-lake-bees-1925-vintage-ballcap');
+});
